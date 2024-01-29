8f8d7b3fafaa7450c88ee4f7279f58a052cf1331
mgcodeur <mgcodeur@gmail.com>
2024-01-29 07:28:22 +0000

    Add bash script for cleaning app

---
+++ clean_app.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Utilisation de rm -rf pour supprimer les répertoires sans confirmation
+rm -rf catalog/ screenshots/
+
+# Afficher un message après la suppression
+echo "Répertoires catalog/ et screenshots/ supprimés avec succès."
